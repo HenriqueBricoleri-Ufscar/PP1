a438752f4356d51b04ec049fea1a99b4c82b87d4
Thiago <thiagokraide2002@gmail.com>
2026-05-13 23:07:27 -0300

    reposicionamento das câmeras

--- src/scene/camera.js
+++ src/scene/camera.js
@@ -5,11 +5,11 @@ export function createCameras(){
     const aspect = window.innerWidth / window.innerHeight;
 
     const camera1 = new THREE.PerspectiveCamera(50, aspect, 0.1, 800);
-    camera1.position.set(0, 0, 10);
+    camera1.position.set(0, 0, 13);
     camera1.lookAt(0,0,0);
     
     const camera2 = new THREE.PerspectiveCamera(50, aspect, 0.1, 800);
-    camera2.position.set(0,-10, 0);
+    camera2.position.set(0,-22, 0);
     camera2.lookAt(0,0,0);
     
     return {camera1, camera2};
